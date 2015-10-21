28247ff0dbe80dec76ae1dd5c9cb99039af58a74
KJWBeige <kevinjustinwilliams@gmail.com>
2015-10-20 18:16:47 -0700

    did prime quiz

--- quiz.rb
+++ quiz.rb
@@ -1,11 +1,23 @@
-def divisor
-
+def divisor(n)          # 100 % ( ) == 0  (1,2,10, 20, etc)
+  arr_divisors=[]
+  for i in 1..n  # two dots excludes n...three dots included n
+    if(n % i == 0)
+      arr_divisors.push(i)
+    end
+  end
+  return arr_divisors
 end
 
-def prime
-
+def prime(n)
+  arr_prime=[]
+  for i in 1..n
+    if(divisor(i).length < 3)
+      arr_prime.push(i)
+    end
+  end
+  return arr_prime
 end
 
-def fib
+#def fib
 
-end
+#end
